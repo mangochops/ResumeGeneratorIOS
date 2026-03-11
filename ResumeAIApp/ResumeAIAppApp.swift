1815fb1da9f6cc1290f95d5e29ccd7f21dec752d
mangochops <whymangoeswillicent@gmail.com>
2026-03-11 14:03:54 +0300

    Initial Commit

---
+++ ResumeAIApp/ResumeAIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeAIAppApp.swift
+//  ResumeAIApp
+//
+//  Created by mac on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ResumeAIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
