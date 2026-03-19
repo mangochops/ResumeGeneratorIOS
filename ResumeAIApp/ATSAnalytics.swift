0d89cadac093674b58650f7f53be58f58ab65563
mangochops <whymangoeswillicent@gmail.com>
2026-03-19 04:50:54 +0300

    ui:adding new pages and functionality

---
+++ ResumeAIApp/ATSAnalytics.swift
@@ -0,0 +1,18 @@
+//
+//  ATSAnalytics.swift
+//  ResumeAIApp
+//
+//  Created by mac on 3/19/26.
+//
+
+import SwiftUI
+
+struct ATSAnalytics: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ATSAnalytics()
+}
---
+++ ResumeAIApp/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  ResumeAIApp
+//
+//  Created by mac on 3/18/26.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
